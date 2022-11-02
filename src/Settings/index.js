7620b6b820604f88edf19bc35475e1c9daf93347
NormanBro <88945718+NormanBro@users.noreply.github.com>
2022-11-02 13:58:01 -0700

    Cors

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,7 +1,10 @@
 const express=require('express');
 const { URL }=require('../Settings/envconf')
+const Cors=require('cors')
 
 const app=express();
+
+const WhiteList=["http://localhost:4200/"]
 
 //Middware
 app.use(express.json());
@@ -10,5 +13,10 @@ app.use(express.urlencoded({extended:false}));
 //rutas
 app.use(URL,require('../Routers/GetBD'));
 app.use(URL,require('../Routers/PostBD'));
+
+//Confg
+app.use(Cors({
+    origin:WhiteList
+}));
 
 module.exports=app;
