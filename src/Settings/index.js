15a0c02c706d3c3c73b331fe0d449167dc744c14
NormanBro <88945718+NormanBro@users.noreply.github.com>
2022-11-02 14:53:41 -0700

    Arreglando cors

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -14,5 +14,9 @@ app.use(URL,require('../Routers/PostBD'));
 
 //Confg
 app.use(Cors());
-
+app.use((req,res)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-with, Content-Type, Accept");
+    
+})
 module.exports=app;
